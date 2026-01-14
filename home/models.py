533c31b1abe6f214bbd8b6621f036c11f38f7872
v_yashchik <medyantsevsvyt@gmail.com>
2026-01-14 17:33:40 +0500

    feat(models): model Task implemented

--- home/models.py
+++ home/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    """Задача ЕГЭ по математике"""
+    text = models.TextField(verbose_name="Текст задачи")
+    answer = models.FloatField(verbose_name="Правильный ответ")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+
+    class Meta:
+        verbose_name = "Задача"
+        verbose_name_plural = "Задачи"
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f"Задача #{self.id}"
