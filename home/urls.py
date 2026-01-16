2cb0b67272086bf68ba95d791bf6c106317f537a
v_yashchik <medyantsevsvyt@gmail.com>
2026-01-17 01:51:25 +0500

    feat(core): problems_by_number page implemented

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('problems/', views.problem_list, name='problem_list'),
+    path('choose/', views.choose_mode, name='choose_mode'),
+    path('variant/', views.full_variant, name='full_variant'),
+    path('check_variant/', views.check_variant, name='check_variant'),
+    path('numbers/', views.all_numbers, name='all_numbers'),
+    path('number/<int:ege_number>/', views.problems_by_number, name='problems_by_number'),
 ]
