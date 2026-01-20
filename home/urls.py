85a407bad8845e9b4904cea60ffa92bfca79ed4f
v_yashchik <medyantsevsvyt@gmail.com>
2026-01-20 13:46:28 +0500

    feat(core): urls corrected

--- home/urls.py
+++ home/urls.py
@@ -5,11 +5,12 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('choose/', views.choose_mode, name='choose_mode'),
     path('variant/', views.full_variant, name='full_variant'),
-    path('check_variant/', views.check_variant, name='check_variant'),
-    path('numbers/', views.all_numbers, name='all_numbers'),
+    path('check-variant/', views.check_variant, name='check_variant'),
     path('result/', views.show_result, name='show_result'),
+    path('numbers/', views.all_numbers, name='all_numbers'),
     path('number/<int:ege_number>/', views.problems_by_number, name='problems_by_number'),
-    # Добавьте эти пути:
-    path('stats/', views.user_statistics, name='user_statistics'),
-    path('global-stats/', views.global_statistics, name='global_statistics'),
+    path('user-statistics/', views.user_statistics, name='user_statistics'),
+    path('global-statistics/', views.global_statistics, name='global_statistics'),
+    path('check-problem/', views.check_problem, name='check_problem'),
+    path('number/<int:ege_number>/', views.problems_by_number, name='problems_by_number')
 ]
